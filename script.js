4b430f1e1132c58e2a62c7eec06fdb70c0382866
Jesse Seward <j_seward5914@hotmail.com>
2023-12-30 16:53:11 -0800

    Completed wiring the JS functionality of the calculator. Works great. Could still work out a couple fine details.

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // assign variables and listen for input. Declare memory, input,
 // current equation and result variables.
+
 var input = "";
 var memoryOne = "";
-var operator = "";
+var currentOperator = "";
 var memoryTwo = "";
-const equation = `${memoryOne}${operator}${memoryTwo}`; 
 var result = "";
-
+ 
 const btnClear = document.querySelector("#button-clear");
 const btn1 = document.querySelector("#button-one");
 const btn2 = document.querySelector("#button-two");
@@ -26,42 +26,62 @@ const btnDivide = document.querySelector("#button-divide");
 const btnEquals = document.querySelector("#button-equals");
 const displayText = document.querySelector("#display-text");
 
-displayText.textContent = input;
+const displayMemoryOne = document.querySelector("#display-memory-one");
+const displayOperator = document.querySelector("#display-operator");
+const displayMemoryTwo = document.querySelector("#display-memory-two");
+const displayResult = document.querySelector("#display-result");
+const html = document.querySelector("html");
 
-// script functions for each button
+const allBtns = document.querySelectorAll("button");
+
+// Create a function to display results to the screen. This will
+// be called after every action performed so it stays current.
+
+function displayResults() {
+    displayMemoryOne.textContent = memoryOne;
+    displayOperator.textContent = currentOperator;
+    displayMemoryTwo.textContent = memoryTwo;
+    displayResult.textContent = result;
+};
 
 // if input is "clear" then clear all memory variables
+
 function clearOut() {
     input = "";
     memoryOne = "";
-    operator = "";
+    currentOperator = "";
     memoryTwo = "";
     result = "";
+    displayResults();
 };
 
-btnClear.addEventListener("click", clearOut());
+btnClear.addEventListener("click", clearOut);
 
 // if input is numbers or decimal, and no operators are present
 // in current equation variable then concat it to first memory
 // variable and display variable on output screen.
 
 function numbersOrDecimal(num) {
-    input += num;
-    displayText.textContent = input;
-
+    if (currentOperator === "") {
+        memoryOne += num;
+        displayResults();
+    } else {
+        memoryTwo += num;
+        displayResults();
+    };
 };
 
-btn0.addEventListener("click", numbersOrDecimal("0"));
-btn1.addEventListener("click", numbersOrDecimal("1"));
-btn2.addEventListener("click", numbersOrDecimal("2"));
-btn3.addEventListener("click", numbersOrDecimal("3"));
-btn4.addEventListener("click", numbersOrDecimal("4"));
-btn5.addEventListener("click", numbersOrDecimal("5"));
-btn6.addEventListener("click", numbersOrDecimal("6"));
-btn7.addEventListener("click", numbersOrDecimal("7"));
-btn8.addEventListener("click", numbersOrDecimal("8"));
-btn9.addEventListener("click", numbersOrDecimal("9"));
-btnDecimal.addEventListener("click", numbersOrDecimal("."));
+btn0.addEventListener("click", () => numbersOrDecimal(0));
+btn1.addEventListener("click", () => numbersOrDecimal(1));
+btn2.addEventListener("click", () => numbersOrDecimal(2));
+btn3.addEventListener("click", () => numbersOrDecimal(3));
+btn4.addEventListener("click", () => numbersOrDecimal(4));
+btn5.addEventListener("click", () => numbersOrDecimal(5));
+btn6.addEventListener("click", () => numbersOrDecimal(6));
+btn7.addEventListener("click", () => numbersOrDecimal(7));
+btn8.addEventListener("click", () => numbersOrDecimal(8));
+btn9.addEventListener("click", () => numbersOrDecimal(9));
+btnDecimal.addEventListener("click", () => numbersOrDecimal("."));
 
 // if input is operator, and second memory variable is empty, 
 // store operator in operator variable. Then
@@ -76,85 +96,60 @@ btnDecimal.addEventListener("click", numbersOrDecimal("."));
 // if current equation variable contains an operator, any new numbers
 // or decimal will go into second memory variable. 
 
-function add() {
-    if (memoryTwo === "") {
-        operator = "+";
-        memoryOne = input;
-        input = "";
-    } else {
-        equals();
-        operator = "+";
-    };
-};
-
-btnAdd.addEventListener("click", add());
-
-function subtract() {
-    if (memoryTwo === "") {
-        operator = "-";
-        memoryOne = input;
-        input = "";
-    } else {
-        equals();
-        operator = "-";
-    };
-};
-
-btnSubtract.addEventListener("click", subtract());
-
-function multiply() {
-    if (memoryTwo === "") {
-        operator = "*";
-        memoryOne = input;
-        input = "";
-    } else {
-        equals();
-        operator = "*";
-    };
-};
-
-btnMultiply.addEventListener("click", multiply());
-
-function divide() {
-    if (memoryTwo === "") {
-        operator = "/";
-        memoryOne = input;
-        input = "";
-    } else {
-        equals();
-        operator = "/";
-    };
-};
-
-btnDivide.addEventListener("click", divide());
-
 // if equals is used then solve the equation and return the result.
 // copy result to first memory variable and display result on output.
-function equals() {
-    if (operator === "+") {
-        result = memoryOne + memoryTwo;
-        memoryOne = result;
+
+btnAdd.addEventListener("click", () => {operate("+");});
+btnSubtract.addEventListener("click", () => {operate("-")});
+btnMultiply.addEventListener("click", () => {operate("*");});
+btnDivide.addEventListener("click", () => {operate("/");});
+btnEquals.addEventListener("click", () => {operate("=")});
+
+function operate(operator) {
+    if (currentOperator === "") {
+        currentOperator = operator;
+        displayResults();
+        return;
+    };
+    if (currentOperator === "+" && operator === "=") {
+        result = +memoryOne + +memoryTwo;
+        displayResults();
+        return;
+    } else if (currentOperator === "-" && operator === "=") {
+        result = +memoryOne - +memoryTwo;
+        displayResults();
+        return;
+    } else if (currentOperator === "*" && operator === "=") {
+        result = +memoryOne * +memoryTwo;
+        displayResults();
+        return;
+    } else if (currentOperator === "/" && operator === "=") {
+        result = +memoryOne / +memoryTwo;
+        displayResults();
+        return;
+    } else if (currentOperator === "+") {
+        memoryOne = +memoryOne + +memoryTwo;
         memoryTwo = "";
-        displayText.textContent = result;
-    } else if (operator === "-") {
-        result = memoryOne - memoryTwo;
-        memoryOne = result;
+        currentOperator = operator;
+        displayResults();
+        return;
+    } else if (currentOperator === "-") {
+        memoryOne = +memoryOne - +memoryTwo;
         memoryTwo = "";
-        displayText.textContent = result;
-    } else if (operator === "*") {
-        result = memoryOne * memoryTwo;
-        memoryOne = result;
+        currentOperator = operator;
+        displayResults();
+        return;
+    } else if (currentOperator === "*") {
+        memoryOne = +memoryOne * +memoryTwo;
         memoryTwo = "";
-        displayText.textContent = result;
-    } else if (operator === "/") {
-        result = memoryOne / memoryTwo;
-        memoryOne = result;
-        memoryTwo = "";
-        displayText.textContent = result;
+        currentOperator = operator;
+        displayResults();
+        return;
     } else {
-        displayText.textContent = memoryOne;
+        memoryOne = +memoryOne / +memoryTwo;
+        memoryTwo = "";
+        currentOperator = operator;
+        displayResults();
+        return;
     }
 };
-
-
-
